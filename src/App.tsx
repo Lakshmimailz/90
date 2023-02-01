765b9d89076fc4ee367dac5bc2aed3e1a5eed0fe
Lakshmi Ponnusamy <lakshmi_mailz@yahoo.com>
2023-02-01 11:23:03 -0500

    90 changes

--- src/App.tsx
+++ src/App.tsx
@@ -4,15 +4,23 @@ import {Ticketsinfo} from './Ticketsinfo';
 import {Imageinfo} from './Imageinfo';
 ;
 
+
   const headingStyle: React.CSSProperties ={
     color:"red",
     textAlign: "center"
 }
 
 function App() {  
-  <h1 style={headingStyle}></h1>
+  
   return (
-  <div>
+  <div
+    style={{ 
+  backgroundImage: "url('https://cdn.pixabay.com/photo/2017/08/30/01/05/milky-way-2695569__480.jpg')",
+  backgroundPosition: 'center',
+  backgroundSize: 'cover',
+  height:"100vh",
+  backgroundRepeat: 'no-repeat'
+}}>
     <h3 style={headingStyle}>Higher Further faster</h3>
     <h1 style={headingStyle} >CAPTAIN MARVEL</h1>
     <h3 style={headingStyle}>In Theatres Now</h3>
